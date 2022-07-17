19508da0b801fa96ddf010cfdb7d923ac281f2dc
OkazakiRui <okazaki200226@gmail.com>
2022-07-17 13:14:49 +0900

    chore: anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use anyhow::{Ok, Result};
+
+fn run() -> Result<()> {
     println!("Hello, world!");
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
